dafe175f15f4ecd0e4ad2a61402572593d081473
Zachariah Toulouse <ztoulouse1@gmail.com>
2016-01-29 18:24:54 -0500

    Updated game.ts

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -14,6 +14,9 @@ function init():void {
     //Creates main display list container
     stage = new createjs.Stage(canvas);
     
+    // Enable mouse events
+    stage.enableMouseOver(20);
+    
     // Set the framerate to 60 frames per second
     createjs.Ticker.setFPS(config.Game.FPS); 
     
